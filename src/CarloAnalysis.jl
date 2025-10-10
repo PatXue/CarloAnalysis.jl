f03fdff95d987f920d5c94365f05755fbb416a2c
PatXue <95881915+PatXue@users.noreply.github.com>
2025-10-09 21:55:30 -0400

    Create initial JobResults struct

--- src/CarloAnalysis.jl
+++ src/CarloAnalysis.jl
@@ -1,5 +1,21 @@
 module CarloAnalysis
 
-# Write your package code here.
+export JobResults
+
+using Carlo.ResultTools
+using DataFrames
+
+struct JobResults
+    jobpath::String     # Path to folder with all job outputs
+    jobname::String     # Name of job in jobpath
+    data::DataFrame     # Parsed data from jobname.results.json
+end
+
+function JobResults(jobpath::String, jobname::String)
+    JobResults(
+        jobpath, jobname,
+        ResultTools.dataframe("$jobpath/$jobname.results.json")
+    )
+end
 
 end
