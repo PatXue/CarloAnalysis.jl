3da4a49153d33d4a50452983c4fc2b24240d5d4f
PatXue <95881915+PatXue@users.noreply.github.com>
2025-10-09 22:13:39 -0400

    Define JobResults in separate module

--- src/CarloAnalysis.jl
+++ src/CarloAnalysis.jl
@@ -5,17 +5,7 @@ export JobResults
 using Carlo.ResultTools
 using DataFrames
 
-struct JobResults
-    jobpath::String     # Path to folder with all job outputs
-    jobname::String     # Name of job in jobpath
-    data::DataFrame     # Parsed data from jobname.results.json
-end
-
-function JobResults(jobpath::String, jobname::String)
-    JobResults(
-        jobpath, jobname,
-        ResultTools.dataframe("$jobpath/$jobname.results.json")
-    )
-end
+include("JobResults.jl")
+include("Plotting.jl")
 
 end
---
+++ src/JobResults.jl
@@ -0,0 +1,16 @@
+module JobResults
+
+struct JobResult
+    jobpath::String     # Path to folder with all job outputs
+    jobname::String     # Name of job in jobpath
+    data::DataFrame     # Parsed data from jobname.results.json
+end
+
+function JobResult(jobpath::String, jobname::String)
+    JobResult(
+        jobpath, jobname,
+        ResultTools.dataframe("$jobpath/$jobname.results.json")
+    )
+end
+
+end
