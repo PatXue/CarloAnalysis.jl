748e7de1b6c4b11621750049962f809795b10f28
PatXue <95881915+PatXue@users.noreply.github.com>
2026-06-04 20:32:09 -0400

    Refactor generate_plot do syntax functionality

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -27,11 +27,6 @@ function generate_plot!(ax::Axis, xs, ys; dots=true, line=true, errors=true, lab
     end
 end
 
-function generate_plot!(f::Function, ax::Axis, xs, ys; kwargs...)
-    g = v -> f(v...)
-    generate_plot!(ax, xs, g.(eachrow(ys)); kwargs...)
-end
-
 function generate_plot!(ax::Axis, x, y, data::AbstractDataFrame; kwargs...)
     generate_plot!(ax, data[:, x], data[:, y]; kwargs...)
 end
@@ -45,6 +40,28 @@ end
 
 function generate_plot!(ax::Axis, x, y, groups, data::DataFrame; kwargs...)
     generate_plot!(ax, x, y, groupby(data, groups); kwargs...)
+end
+
+
+# Apply a function transformation to the data before plotting
+function generate_plot!(f::Function, ax::Axis, xs, ys; kwargs...)
+    g = v -> f(v...)
+    generate_plot!(ax, xs, g.(eachrow(ys)); kwargs...)
+end
+
+function generate_plot!(f::Function, ax::Axis, x, y, data::AbstractDataFrame; kwargs...)
+    generate_plot!(f, ax, data[:, x], data[:, y]; kwargs...)
+end
+
+function generate_plot!(f::Function, ax::Axis, x, y, grouped_data::GroupedDataFrame; kwargs...)
+    for key in keys(grouped_data)
+        data = grouped_data[key]
+        generate_plot!(f, ax, x, y, data; label="$(NamedTuple(key))", kwargs...)
+    end
+end
+
+function generate_plot!(f::Function, ax::Axis, x, y, groups, data::DataFrame; kwargs...)
+    generate_plot!(f, ax, x, y, groupby(data, groups); kwargs...)
 end
 
 end
